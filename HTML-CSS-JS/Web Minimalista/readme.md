f749986e41d704b45df0adb33721b34bfb1df045
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2023-02-24 13:20:04 +0100

    Página Web Minimalista

---
+++ HTML-CSS-JS/Web Minimalista/readme.md
@@ -0,0 +1,3 @@
+# Página web minimalista
+
+En este prompt, le pedí a ChatGPT "Una página web creada con HTML, CSS y JavaScript que sea minimalista.
---
+++ HTML-CSS-JS/Web Minimalista/script.js
@@ -0,0 +1,3 @@
+console.log('Hola mundo');
+
+// Esto no es lo que yo pensaba que me iba a hacer, pensé que pondría alguna característica a la web con JS xD
